869344b9d9dce994c7b259725b8ba00444b71806
VARUN AGGARWAL <117046986+VarunAggarwal1998@users.noreply.github.com>
2023-11-11 05:41:10 -0500

    Update signIn.py

--- D2/Code/Components/signIn.py
+++ D2/Code/Components/signIn.py
@@ -1,94 +1,74 @@
-# Importing necessary modules
+# Importing necessary modules for creating the GUI
 import tkinter as tk
 from tkinter import messagebox, font
-from statWindow import MainWindow 
+# Importing the Image modules from Pillow to handle background images
+from PIL import Image, ImageTk
+# Importing the main application window class
+from startWindow import MainWindow
 
 class SignInWindow:
-    """
-    A class used to represent the SignInWindow.
-
-    ...
-
-    Attributes
-    ----------
-    root : Tk
-        The root window of the application.
-    username : Entry
-        The entry widget for the username.
-    password : Entry
-        The entry widget for the password (masked).
-    sign_in_button : Button
-        The button that triggers the sign-in functionality.
-
-    Methods
-    -------
-    sign_in():
-        Checks the credentials and displays a message.
-    """
+    """A class to create and manage the sign-in window of the application."""
 
     def __init__(self, root):
-        """
-        Constructs all the necessary attributes for the SignInWindow object.
-
-        Parameters
-        ----------
-            root : Tk
-                The root window of the application.
-        """
-
+        """Initialize the sign-in window with a background image, entry fields, and buttons."""
+        # Set the root window and its properties
         self.root = root
-        self.root.title('Sign In')
+        self.root.title('Sign In')  # Window title
+        self.root.geometry('1024x768')  # Window size
 
-        # Set the geometry for the window (width x height)
-        self.root.geometry('300x400')  # You might want to adjust the size
+        # Load and set the background image
+        image = Image.open('image2.jpeg')
+        self.background_image = ImageTk.PhotoImage(image)  # Convert to PhotoImage
+        background_label = tk.Label(self.root, image=self.background_image)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)  # Place the image
+        background_label.lower()  # Ensure it's in the background
 
-        # Set the background color of the window
-        bgColor = "#f4f4f4"  # Light grey background, easier on the eyes and more professional
-        self.root.configure(bg=bgColor)
+        # Define font styles for widgets
+        labelFont = font.Font(family="Helvetica", size=18)
+        headingFont = font.Font(family="Helvetica", size=50, weight="bold")
+        buttonFont = font.Font(family="Helvetica", size=18, weight="bold")
 
-        # Define a style for the labels and buttons
-        labelFont = font.Font(family="Helvetica", size=12)
-        buttonFont = font.Font(family="Helvetica", size=12, weight="bold")
+        # Create and place the heading label
+        heading_label = tk.Label(self.root, text="METRICSTICS", fg="cyan", font=headingFont, bg=self.root.cget('bg'))
+        heading_label.pack(pady=(10, 20))
 
-        # Creating the username and password labels and entry boxes
-        tk.Label(self.root, text="Username", bg=bgColor, fg="black", font=labelFont).pack(pady=(20, 0))
-        self.username = tk.Entry(self.root, bd=2, relief="flat")  # flat relief with border creates a modern text field style
-        self.username.pack(pady=10, padx=20, fill='x')  # fill x-axis, more space to type
+        # Create and place the username label and entry box
+        tk.Label(self.root, text="Username", fg="white", font=labelFont).pack(pady=(20, 0))
+        self.username = tk.Entry(self.root, bd=2, relief="flat", bg="white", fg="black", font=labelFont, width=30)
+        self.username.pack(pady=(10, 20), padx=20)
 
-        tk.Label(self.root, text="Password", bg=bgColor, fg="black", font=labelFont).pack(pady=(10, 0))
-        self.password = tk.Entry(self.root, show="*", bd=2, relief="flat")
-        self.password.pack(pady=10, padx=20, fill='x')
+        # Create and place the password label and entry box
+        tk.Label(self.root, text="Password", fg="white", font=labelFont).pack(pady=(10, 0))
+        self.password = tk.Entry(self.root, show="*", bd=2, relief="flat", bg="white", fg="black", font=labelFont, width=30)
+        self.password.pack(pady=(10, 20), padx=20)
 
-        # Creating the sign-in button
-        self.sign_in_button = tk.Button(self.root, text="Sign In", command=self.sign_in, bg="#28a745", fg="black", font=buttonFont, bd=0, relief="flat", padx=10, pady=5)
+        # Create and place the sign-in button
+        self.sign_in_button = tk.Button(self.root, text="Sign In", command=self.sign_in, bg="White", fg="Black",
+                                        font=buttonFont, bd=0, relief="flat", padx=10, pady=5)
         self.sign_in_button.pack(pady=20)
 
     def sign_in(self):
-        """
-        Handles the sign-in functionality by checking the credentials.
-        If the credentials match, it displays a successful login message; otherwise, it shows an error.
-        """
-
+        """Handle the sign-in action."""
+        # Get username and password from the entry fields
         username = self.username.get()
         password = self.password.get()
 
-        # Placeholder for actual authentication logic (e.g., checking a database)
+        # Check credentials (placeholder for actual authentication logic)
         if username == "admin" and password == "password":
-            # messagebox.showinfo("Success", "Sign-in successful!")
-
-            # Close the current sign-in window
-            self.root.destroy()
+            # If login is successful, hide the sign-in window
+            self.root.withdraw()
 
             # Open the main application window
-            new_root = tk.Tk()  # Create a new Tk instance
-            main_app = MainWindow(new_root)  # Create an instance of the MainWindow
-            new_root.mainloop()  # Run the main application event loop
+            main_app_window = tk.Toplevel(self.root)
+            main_app = MainWindow(main_app_window)
+            main_app_window.mainloop()  # Start the main loop for the main application window
         else:
+            # If credentials are incorrect, show an error message
             messagebox.showerror("Error", "Invalid credentials. Please try again.")
 
-
-# Usage: Creating a window and running the application
+# The following code runs if this script is the main program and not a module imported by another script
 if __name__ == "__main__":
-    root = tk.Tk()  # Create a standard Tk instance
-    SignInWindow(root)  # Create an instance of the SignInWindow
-    root.mainloop()  # Run the tkinter event loop
+    # Create the root window and pass it to the sign-in window class
+    root = tk.Tk()
+    SignInWindow(root)
+    root.mainloop()  # Start the Tkinter event loop
